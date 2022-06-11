a36242a57206518874e1452542a54267c321448d
bcsendes <53707647+bcsendes@users.noreply.github.com>
2022-06-11 13:23:07 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start services
+echo Starting services
+service ssh start
+/etc/init.d/node-red start
+
+echo $$
+
+# -$$: kill process group (parent and children)
+#trap 'trap - TERM; kill 0' TERM
+#trap 'trap - INT TERM; kill 0' INT TERM
+
+trap 'trap - TERM; kill -s TERM -- -$$' TERM
+
+tail -f /dev/null & wait
+
+# Stop services
+echo Stopping services
+service ssh stop
+/etc/init.d/node-red stop
+
+exit 0
