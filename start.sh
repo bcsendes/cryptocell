11837c165bc5ac81f568de11e7f61d5dbadfb9f6
bcsendes <53707647+bcsendes@users.noreply.github.com>
2022-07-16 19:01:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,11 +8,15 @@ echo Starting services
 if [ -f /home/cryptogt/postgresql.conf ]; then 
     unlink /home/cryptogt/postgresql.conf 
 fi
-ln -s /var/lib/postgresql/data/postgresql.conf /home/cryptogt/postgresql.conf
+ln -s "$PGDATA/postgresql.conf" /home/cryptogt/postgresql.conf
 if [ -f /home/cryptogt/settings.js ]; then 
     unlink /home/cryptogt/settings.js
 fi
 ln -s /home/cryptogt/.node-red/settings.js /home/cryptogt/settings.js
+if [ -f /home/cryptogt/data ]; then 
+    unlink /home/cryptogt/data
+fi
+ln -s "$PGVOLUME" /home/cryptogt/data
 # Starting services
 service ssh start
 /etc/init.d/node-red.sh start
